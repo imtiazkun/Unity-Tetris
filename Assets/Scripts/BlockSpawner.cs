ee7d5e29e2e0a8efbdb6eee0667e84a3297e43bc
imtiazkun <alshahriar.me@gmail.com>
2022-04-29 23:34:37 +0600

    Gameplay finished

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -1,17 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BlockSpawner : MonoBehaviour
 {
     public GameObject[] blocks;
+
+    public int Score = 0;
+    public GameObject ScoreDisplay;
+    public GameObject PraiseBoard;
     // Start is called before the first frame update
     void Start()
     {
         Generate();
+        InvokeRepeating("TimeScore", 2.0f, 0.3f);
+    }
+
+    public void StopInvoke () {
+        CancelInvoke();
     }
 
     public void Generate () {
         Instantiate(blocks[Random.Range(0, blocks.Length)], transform.position, Quaternion.identity);
+    }
+
+    void TimeScore () {
+        AddScore(5);
+    }
+
+    public void AddScore (int amount) {
+        int hundredth = 0;
+        int thousandth = 0;
+
+        Score += amount;
+        
+        hundredth = Score;
+        thousandth = Score;
+
+        ScoreDisplay.GetComponent<TextMeshPro>().text = Score.ToString();
+        
+        if (hundredth == 100) {
+            PraiseBoard.GetComponent<TextMeshPro>().text = "Going Smooth!!";
+            hundredth = 0;
+        } else if (thousandth == 1000) {
+            PraiseBoard.GetComponent<TextMeshPro>().text = "Going Big!!";
+            thousandth = 0;
+        } else {
+            PraiseBoard.GetComponent<TextMeshPro>().text = "Great Going!!";
+        }
+    }
+
+
+    public void SetPraiseBoardMessage (string message) {
+        PraiseBoard.GetComponent<TextMeshPro>().text = message;
     }
 }
--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -13,6 +13,11 @@ public class controller : MonoBehaviour
 
     public static int width = 15;
     public static int height = 20;
+    public static Transform[,] grid = new Transform[width, height];
+
+
+
+    public int points_on_line_completion = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -48,8 +53,11 @@ public class controller : MonoBehaviour
             transform.position += new Vector3(0, -1, 0);
             if (!IsMoveValid()) {
                 transform.position -= new Vector3(0, -1, 0);
+                gameObject.tag = "Inactive";
+                AddToGrid();
+                CheckGameStatus();
+                CheckForLines();
                 this.enabled = false;
-                FindObjectOfType<BlockSpawner>().Generate();
             }
             previous_time = Time.time;
         }
@@ -67,10 +75,100 @@ public class controller : MonoBehaviour
                 return false;
             }
 
-
-            Debug.Log(roundedX +" "+ roundedY);
+            if (grid[roundedX, roundedY] != null) {
+                return false;
+            }
         }
 
         return true;
     }
+
+
+    void AddToGrid () {
+        foreach (Transform children in transform) {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+
+    void CheckGameStatus () {
+        if (HasExceededHeight()) {
+            FindObjectOfType<BlockSpawner>().StopInvoke();
+            FindObjectOfType<BlockSpawner>().SetPraiseBoardMessage("Game Over!!");
+        } else {
+            FindObjectOfType<BlockSpawner>().Generate();
+        }
+    }
+
+    /** 
+    *   BOOLEAN FUNCTION
+    *   check if block has a block below and above is height limit
+    */
+    bool HasExceededHeight () {
+        foreach (Transform children in transform) {
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+            if (roundedY >= height - 2) {
+                if (gameObject.tag == "Inactive") {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void CheckForLines () {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i)) {
+                DeleteLine(i);
+                RowDown(i);
+                FindObjectOfType<BlockSpawner>().AddScore(points_on_line_completion);
+            }
+        }
+    }
+
+
+    // BOOLEAN FUNCTION
+    /**
+    *   - Checks through array if there is even a single empty cell
+    *   - Returns true by default and only returns false if empty cell available
+    */
+    bool HasLine (int i) {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void DeleteLine (int i) {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j,i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+
+    void RowDown (int i) {
+        Debug.Log("Row going Down");
+        for (int y = i; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, y] != null) {
+                    grid[x, y - 1] = grid[x, y];
+                    grid[x, y] = null;
+                    grid[x, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
 }
